c2ed7d98bb765387dff4ef02acec11ad76646163
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2023-07-06 14:11:20 -0500

    Added updates.py as python file

---
+++ updates.py
@@ -0,0 +1,19 @@
+import subprocess
+
+# Run 'sudo apt update'
+subprocess.run(['sudo', 'apt', 'update'])
+
+# Run 'sudo apt full-upgrade -y'
+subprocess.run(['sudo', 'apt', 'full-upgrade', '-y'])
+
+# Run 'sudo apt dist-upgrade -y'
+subprocess.run(['sudo', 'apt', 'dist-upgrade', '-y'])
+
+# Run 'sudo apt autoremove -y'
+subprocess.run(['sudo', 'apt', 'autoremove', '-y'])
+
+# Run 'sudo apt clean -y'
+subprocess.run(['sudo', 'apt', 'clean', '-y'])
+
+# Run 'sudo apt purge -y'
+subprocess.run(['sudo', 'apt', 'purge', '-y'])
