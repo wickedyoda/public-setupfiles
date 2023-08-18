583b51c4058ec22c99aff5f22f0eedee55fd05fd
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2023-08-18 14:03:13 -0500

    Update updates.py
    Updated updates.py

--- python_scripts/updates.py
+++ python_scripts/updates.py
@@ -3,6 +3,9 @@ import subprocess
 # Run 'sudo apt update'
 subprocess.run(['sudo', 'apt', 'update'])
 
+# Run 'sudo apt upgrade -y'
+subprocess.run(['sudo', 'apt', 'upgrade', '-y'])
+
 # Run 'sudo apt full-upgrade -y'
 subprocess.run(['sudo', 'apt', 'full-upgrade', '-y'])
 
