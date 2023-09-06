281f0be34a6ba6f0f7a6391cf0fbdc9654418b10
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2023-09-06 14:12:32 -0500

    Update echo line

--- ubuntu-debian-based/setup_cron_job_updates.sh
+++ ubuntu-debian-based/setup_cron_job_updates.sh
@@ -15,8 +15,13 @@ sudo systemctl start cron
 sudo systemctl stop cron
 
 # Add the update and upgrade cron job to /etc/cron.d/
-echo 'MAILTO="alerts@tyates.one"
-0 */6 * * * root apt-get update && apt-get -y -d full-upgrade && apt-get autoremove -y && sudo apt-get clean -y && sudo apt-get purge -y' | sudo tee /etc/cron.d/auto_updates
+# Old line that doesnt work
+#   echo 'MAILTO="alerts@tyates.one"
+#   0 */6 * * * root apt-get update && apt-get -y -d full-upgrade && apt-get autoremove -y && sudo apt-get clean -y && sudo apt-get purge -y' | sudo tee /etc/cron.d/auto_updates
+
+# Added new line to test.
+echo '0 */6 * * * root apt-get update && apt-get -y -d full-upgrade && apt-get autoremove -y && sudo apt-get clean -y && sudo apt-get purge -y' | sudo tee -a /etc/crontab
+
 
 # Restart the cron service to apply the new cron job
 sudo systemctl start cron
