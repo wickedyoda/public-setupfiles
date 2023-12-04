59c910462c0b67c7ecd7fc90434fa5d7cbe7eb5d
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2023-12-04 11:44:01 -0600

    Change raspberry pi apps file
    Changed raaspberry pi install apps file
    moved raspbery pi fstab to seperate file

--- raspberrypi/rasp_pi_install_apps.sh
+++ raspberrypi/rasp_pi_install_apps.sh
@@ -36,41 +36,10 @@ sudo apt-get -y install \
   putty \
   gpart \
   deja-dup \
-  chromium-browser \
   git \
   curl \
   libgconf-2-4 
 
-# ----------------------------------------------------------------------
-# | Install Discord                  |
-# ----------------------------------------------------------------------
-
-cd /home/traver/Downloads 
-wget -O discord-0.0.1.deb https://discordapp.com/api/download?platform=linux&format=deb
-sudo dpkg -i discord-0.0.1.deb
-
-
-# ----------------------------------------------------------------------
-# | creates folders for shares, Install fstab entries, maps mounts to nas|
-# ----------------------------------------------------------------------
-
-sudo cp /etc/fstab /etc/ftab.orig.backup
-
-# cd ~/home/pi
-
-sudo mkdir /home/pi/naspublic
-sudo mkdir /home/pi/nasdownloadedmedia
-
-sudo echo "//nas/public  /home/pi/naspublic  cifs  vers=2.0,username=admin,password=mypassword,file_mode=0777,dir_mode=0777 0 0
-//nas/DownloadedMedia  /home/pi/nasdownloadedmedia  cifs vers=2.0,username=traver,password=mypassword,file_mode=0777,dir_mode=0777 0 0" >> /etc/fstab
-
-sudo mount -a
-
-# ----------------------------------------------------------------------
-# | Copy OpenVPN configs           |
-# ----------------------------------------------------------------------
-
-cp "smb://NAS/public/Raspberry Pi Setups and Files/mattvpn.ovpn" /home/traver/mattvpn.ovpn
 
 # ----------------------------------------------------------------------
 # | Install plugin for Netflix and video play on Pi                    |
@@ -86,4 +55,6 @@ sh ventz-media-pi
 sudo apt-get -y update
 sudo apt-get -y dist-upgrade
 sudo apt-get -y full-upgrade
-sudo apt -y autoremove 
+sudo apt -y autoremove
+sudo apt -y clean 
+sudo apr -y purge
