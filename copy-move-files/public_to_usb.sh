4822905f0e2648828d4039654892a456cfdc6573
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2023-12-30 03:30:00 -0600

    Fixing dir path

--- copy-move-files/public_to_usb.sh
+++ copy-move-files/public_to_usb.sh
@@ -1,3 +1,3 @@
 sudo mount -a
 
-rsync -r -E -z -u --progress --exclude '/vm' --exclude '/proxmox' --delete  /home/pi/naspublic/ /media/pi/4tb/public
+rsync -r -E -z -u --progress --exclude '/vm' --exclude '/proxmox' --delete  /home/traver/naspublic/ /media/traver/4tb/public
