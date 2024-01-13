a9e5c171a69835a7f865243f013e88e44553972d
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2024-01-13 13:27:18 -0600

    fstab update

--- fstab-setup/setup-fstab.sh
+++ fstab-setup/setup-fstab.sh
@@ -17,8 +17,8 @@ mkdir nasmedia
 mkdir naspublic
 
 # Echo the command below into the file /etc/fstab
-echo '//nas/public  /home/naspublic  cifs  vers=2.0,username=admin,password=,file_mode=0777,dir_mode=0777 0 0
-//nas/DownloadedMedia  /home/nasmedia  cifs  vers=2.0,username=admin,password=,file_mode=0777,dir_mode=0777 0 0' | sudo tee -a /etc/fstab
+echo '//nas/public  /home/naspublic  cifs  vers=2.0,username=admin,password=,file_mode=0777,dir_mode=0777,auto 0 0
+//nas/DownloadedMedia  /home/nasmedia  cifs  vers=2.0,username=admin,password=,file_mode=0777,dir_mode=0777,auto 0 0' | sudo tee -a /etc/fstab
 
 
 
