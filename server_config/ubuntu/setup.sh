bd56c74aa0a616ca9d693b25263a54cc40aaf25a
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2024-03-05 10:54:41 -0600

    s

--- server_config/ubuntu/setup.sh
+++ server_config/ubuntu/setup.sh
@@ -3,10 +3,10 @@
 # initial install
 sudo apt-get update -y
 
-#installing git
+# installing git
 sudo apt install git -y
 
-#mkdir for git repo
+# mkdir for git repo
 sudo mkdir /home/traver/public-setupfiles
 cd /home/traver/public-setupfiles
 git init
@@ -14,11 +14,11 @@ git remote add origin https://github.com/wickedyoda/public-setupfiles
 git pull
 git checkout main
 
-#Change permissions
+# Change permissions
 sudo chmod +x -R /home/traver/public-setupfiles
 
 # Run full updates first time
 sudo /home/traver/public-setupfiles/updates/updates.sh
 
-#Serer apps setup
+# Serer apps setup
 sudo apt-get -y install cockpit samba-common-bin openssh-server smbclient cifs-utils exfat-fuse curl unattended-upgrades cron-apt git curl
