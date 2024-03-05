a1d92f02b20ace249adf0b7b66341d0198d9aa36
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2024-03-05 15:01:59 -0600

    fgdh

--- updates/dist-upgrade.sh
+++ updates/dist-upgrade.sh
@@ -1,6 +1,10 @@
+# /bin/bash
+
 sudo apt update
 sudo apt upgrade -y
 sudo apt full-upgrade -y
+sudo apt dist-upgrade -y
 sudo apt autoremove -y
 sudo apt clean -y
 sudo apt purge -y
+echo "System update and cleanup complete!"
