6ed5bcee3ad71c4c546a6aa643685980e06ecac9
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2024-03-15 12:36:11 -0500

    ss
    Updates script for desktop

--- batch_files/Downloads Backup.bat
+++ batch_files/Downloads Backup.bat
@@ -1,4 +1,4 @@
 robocopy C:\Users\trave\Downloads \\NAS\public\downloads /z /s /r:2 /w:3 /copy:d /XD "C:\Users\trave\Downloads\VMs" /xf *.jpg /xf *.iso /xf *.png /xf *.jpeg /xf *.docx /xf *.pdf
 
-robocopy d:\Downloads \\NAS\public\downloads /z /s /r:2 /w:3 /copy:d /XD "C:\Users\trave\Downloads\VMs" /xf *.jpg /xf *.iso /xf *.png /xf *.jpeg /xf *.docx /xf *.pdf
+
 
--- batch_files/Traver-desk VM Backup.bat
+++ batch_files/Traver-desk VM Backup.bat
@@ -1,1 +1,5 @@
-robocopy C:\Users\trave\Downloads\vms \\NAS\public\vm\traver-desk /r:2 /w:2 /purge
+net use w: \\nas\public
+net use s: \\nas\downloadedmedia 
+net use r: \\nas\downloads 
+
+robocopy C:\Users\trave\Downloads\VMs \\NAS\public\vm\traver-desk /z /s /r:2 /w:2 /copy:dt /purge /compress
