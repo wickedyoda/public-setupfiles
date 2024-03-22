c1b87109ec55c6caf64fa1fa3460072e4c20345d
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2024-03-22 02:50:09 -0500

    added -y for docker install

--- server_config/debian/debian-docker-install.sh
+++ server_config/debian/debian-docker-install.sh
@@ -1,7 +1,7 @@
  for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
 sudo apt-get purge docker.io docker-doc docker-compose podman-docker containerd runc
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -10,5 +10,5 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
