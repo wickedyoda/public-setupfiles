4b4f5d216d8f0357f3d4767745d5b1af932bf683
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2024-05-04 18:10:50 -0500

    w

--- batch_files/Downloads Backup.bat
+++ batch_files/Downloads Backup.bat
@@ -1,4 +1,9 @@
-robocopy C:\Users\trave\Downloads \\NAS\public\downloads /z /s /r:2 /w:3 /copy:d /XD "C:\Users\trave\Downloads\VMs" /xf *.jpg /xf *.iso /xf *.png /xf *.jpeg /xf *.docx /xf *.pdf
+net use w: \\nas\public
+net use s: \\nas\downloadedmedia
+net use r: \\nas\downloads
+net use t: \\NAS\public-share2
+
+robocopy C:\Users\trave\Downloads \\nas\public\downloads /z /s /r:2 /w:3 /XD "C:\Users\trave\Downloads\VMs" /xf *.jpg /xf *.iso /xf *.png /xf *.jpeg /xf *.docx /xf *.pdf
 
 
 
