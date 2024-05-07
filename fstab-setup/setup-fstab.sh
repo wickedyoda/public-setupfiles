b08588a3df706db96b9cf5671500b4c8d7744486
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2024-05-07 14:48:25 -0500

    Updated Fstab
    Updated Fstab

--- fstab-setup/setup-fstab.sh
+++ fstab-setup/setup-fstab.sh
@@ -18,10 +18,12 @@ apt-get -y install \
 cd /media
 mkdir nasmedia
 mkdir naspublic
+mkdir naspublic-share2
 
 # Echo the command below into the file /etc/fstab
 echo '//nas/public  /media/naspublic  cifs  vers=2.0,username=admin,password=$PASSWORD,file_mode=0777,dir_mode=0777,_netdev,auto 0 0
-//nas/DownloadedMedia  /media/nasdownloadedmedia  cifs  vers=2.0,username=admin,password=$PASSWORD,file_mode=0777,dir_mode=0777,_netdev,auto 0 0' | sudo tee -a /etc/fstab
+//nas/DownloadedMedia  /media/nasdownloadedmedia  cifs  vers=2.0,username=admin,password=$PASSWORD,file_mode=0777,dir_mode=0777,_netdev,auto 0 0
+//nas/public-share2 /media/naspublic-share2  cifs  vers=2.0,username=admin,password=$PASSWORD,file_mode=0777,dir_mode=0777,_netdev,auto 0 0' | sudo tee -a /etc/fstab
 
 # Mount the file systems
 mount -a
