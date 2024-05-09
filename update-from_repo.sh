d8f89c99febaebef6a1aa8683f4f3216b5f0790e
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2024-05-09 16:22:56 -0500

    Last update

--- update-from_repo.sh
+++ update-from_repo.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
+# run apt update to make sure the system is got latest repo files
+sudo apt update -y
+
+# Check if git is installed
+if ! command -v git &> /dev/null; then
+    # Install git
+    sudo apt-get install git -y
+fi
+
 # Specify the remote repository URL
 remote_repo="https://github.com/wickedyoda/public-setupfiles.git"
 
 # Specify the local file path
-local_file="/home/traver/pubic-setupfiles"
+local_file="./pubic-setupfiles"
 
 # Check if the local file path exists and is not empty
 if [ -d "$local_file" ] && [ -n "$(ls -A $local_file)" ]; then
     # Delete the existing directory
-    rm -rf "$local_file"
+    sudo rm -rf "$local_file"
 fi
 
 # Clone the remote repository to the local file path
 git clone "$remote_repo" "$local_file"
 
-cd /home/traver
-chmod +x ./public-setupfiles/*.sh
+# Make all files executable and public ownership
+sudo chmod 777 -R $local_file
+
