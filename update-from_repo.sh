9d273956c2f89ef064069f5ec5bac9d93143caf0
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2024-05-09 16:09:58 -0500

    s

--- update-from_repo.sh
+++ update-from_repo.sh
@@ -4,7 +4,7 @@
 remote_repo="https://github.com/wickedyoda/public-setupfiles.git"
 
 # Specify the local file path
-local_file="./pubic-setupfiles"
+local_file="/home/traver/pubic-setupfiles"
 
 # Check if the local file path exists and is not empty
 if [ -d "$local_file" ] && [ -n "$(ls -A $local_file)" ]; then
@@ -14,3 +14,6 @@ fi
 
 # Clone the remote repository to the local file path
 git clone "$remote_repo" "$local_file"
+
+cd /home/traver
+chmod +x ./public-setupfiles/*.sh
