cb04d34837baae6efe61bd3ab6d01c2adc1ece85
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2024-05-09 15:10:53 -0500

    Update familymedicalbackup.bat

--- batch_files/familymedicalbackup.bat
+++ batch_files/familymedicalbackup.bat
@@ -2,4 +2,6 @@ net use w: \\nas\public
 
 robocopy "C:\Users\Traver\OneDrive\Family Health Docs" "\\nas\public\Health Documents" /z /s /r:2 /w:1 /purge
 
-robocopy "C:\Users\Trave\OneDrive\Family Health Docs" "\\nas\public\Health Documents" /z /s /r:2 /w:1 /purge
+robocopy "C:\Users\Trave\OneDrive\Family Health Docs" "C:\Users\trave\Nextcloud\Health Documents" /z /s /r:2 /w:1 /purge
+
+robocopy "C:\Users\trave\Nextcloud\Health Documents" "C:\Users\Trave\OneDrive\Family Health Docs" /z /s /r:2 /w:1 
