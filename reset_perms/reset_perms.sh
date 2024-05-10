6508190bdc5c6ed4550db417e626088168de5685
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2024-05-10 18:29:48 -0500

    Update reset_perms.sh
    xzczx

--- reset_perms/reset_perms.sh
+++ reset_perms/reset_perms.sh
@@ -1,4 +1,13 @@
-cd /home/traver
+cd /home/traver/drives
+
+sudo mount /dev/sde1 ./sde-public1/
+
+sudo mount /dev/sdd1 ./sdd-public1-clone/
+
+sudo mount /dev/sdc1 ./sdc-public2
+
+sudo mount /dev/sdb1 ./sdb-public2-clone/
+
 sudo mount /dev/sdb1 ./datadrive
 sudo chmod 777 -R ./datadrive/public/
 sudo chmod 777 -R ./datadrive/DownloadedMedi/
