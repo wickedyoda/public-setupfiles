52e5f2bf9f6f61d5cdee07b3b23edbdeb1723306
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2024-06-27 13:23:05 -0500

    sdsad

--- batch_files/Copy cricut from cloud to nextcloud.bat
+++
@@ -1,2 +0,0 @@
-
-robocopy C:\Users\trave\OneDrive\Cricut C:\Users\trave\Nextcloud\Cricut /z /s /r:2 /copy:dt /w:2 
--- batch_files/Downloads Backup.bat
+++ batch_files/Downloads Backup.bat
@@ -3,7 +3,8 @@ net use s: \\nas\downloadedmedia
 net use r: \\nas\downloads
 net use t: \\NAS\public-share2
 
-robocopy C:\Users\trave\Downloads \\nas\public\downloads /z /s /r:2 /w:3 /XD "C:\Users\trave\Downloads\VMs" /xf *.jpg /xf *.iso /xf *.png /xf *.jpeg /xf *.docx /xf *.pdf
+robocopy C:\Users\trave\Downloads \\nas\downloads /z /s /r:2 /w:3 /XD "C:\Users\trave\Downloads\VMs" /xf *.jpg /xf *.iso /xf *.png /xf *.jpeg /xf *.docx /xf *.pdf
 
+robocopy F:\Downloads \\nas\downloads /z /s /r:2 /w:3 /XD "F:\Downloads\VMs" /xf *.jpg /xf *.iso /xf *.png /xf *.jpeg /xf *.docx /xf *.pdf /xf *.txt /xf *.doc /xf *.mp4 /xf *.mkv
 
 
