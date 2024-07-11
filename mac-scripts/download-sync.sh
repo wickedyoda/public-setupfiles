57846646e83da765722551adf0e7afd0d8115190
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2024-07-11 11:10:20 -0500

    Macdownloads1 test

--- mac-scripts/download-sync.sh
+++ mac-scripts/download-sync.sh
@@ -1,1 +1,3 @@
-rsync -r -v /Users/traver/Downloads/ "smb://traver@nas/mac-downloads"
+if [ -d "/Users/traver/Downloads/" ]; then
+    rsync -r -v /Users/traver/Downloads/ "smb://traver@nas/mac-downloads/apps/"
+fi
