b8d6fd9c0a8c4435a56b2d1504de36defc90b948
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2024-07-11 11:05:04 -0500

    df

--- batch_files/Traver-desk VM Backup.bat
+++ batch_files/Traver-desk VM Backup.bat
@@ -2,4 +2,4 @@ net use w: \\nas\public
 net use s: \\nas\downloadedmedia 
 net use r: \\nas\downloads 
 
-robocopy C:\Users\trave\Downloads\VMs \\NAS\public-share2\vm\traver-desk /z /s /r:2 /w:2 /copy:dt /purge /compress
+robocopy F:\Downloads\VMs \\NAS\public-share2\vm\traver-desk /z /s /r:2 /w:2 /copy:dt /purge /compress
