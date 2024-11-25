d3af42384e6fbb2ba91ac56ce234f7abf5832b10
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2024-11-24 18:46:51 -0600

    updated backups

--- batch_files/Backup Videos.bat
+++ batch_files/Backup Videos.bat
@@ -1,2 +1,4 @@
 robocopy C:\Users\trave\Videos\ "\\nas\public\Traver's Personal\produced_videos" /z /s /r:2 /copy:dt /w:2 /purge
 robocopy "\\nas\public\Traver's Personal\produced_videos" C:\Users\trave\Videos\ /z /s /r:2 /copy:dt /w:2 
+
+robocopy C:\Users\trave\Videos\ "\\NAS\mymedia\Videos" /z /s /r:2 /copy:dt /w:2
--- batch_files/Downloads Backup.bat
+++ batch_files/Downloads Backup.bat
@@ -5,6 +5,6 @@ net use t: \\NAS\public-share2
 
 robocopy C:\Users\trave\Downloads \\nas\downloads /z /s /r:2 /w:3 /XD "C:\Users\trave\Downloads\VMs" /xf *.jpg /xf *.iso /xf *.png /xf *.jpeg /xf *.docx /xf *.pdf
 
-robocopy F:\Downloads \\nas\downloads /z /s /r:2 /w:3 /XD "F:\Downloads\VMs" /xf *.jpg /xf *.iso /xf *.png /xf *.jpeg /xf *.docx /xf *.pdf /xf *.txt /xf *.doc /xf *.mp4 /xf *.mkv
+
 
 
