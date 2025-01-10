b746a991b0792aebbd86060285c713dd297269a2
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2025-01-10 17:59:54 -0600

    Update Downloads Backup.bat
    Update downloads file

--- batch_files/Downloads Backup.bat
+++ batch_files/Downloads Backup.bat
@@ -3,7 +3,7 @@ net use s: \\nas\downloadedmedia
 net use r: \\nas\downloads
 net use t: \\NAS\public-share2
 
-robocopy C:\Users\trave\Downloads \\nas\downloads /z /s /r:2 /w:3 /XD "C:\Users\trave\Downloads\VMs" /xf *.jpg /xf *.iso /xf *.png /xf *.jpeg /xf *.docx /xf *.pdf
+robocopy C:\Users\trave\Downloads \\nas\downloads /z /s /r:2 /w:3 /XD "C:\Users\trave\Downloads\VMs" /xf *.jpg *.iso *.png *.jpeg *.docx *.pdf /COPY:DAT
 
 
 
