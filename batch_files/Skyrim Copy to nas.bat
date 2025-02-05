063ac9e446a631d5ac33bb1a4522f8106a2c106c
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2025-02-05 13:07:03 -0600

    Update Skyrim Copy to nas.bat

--- batch_files/Skyrim Copy to nas.bat
+++ batch_files/Skyrim Copy to nas.bat
@@ -1,4 +1,4 @@
 
-robocopy "C:\Program Files (x86)\Steam\steamapps\common\Skyrim Special Edition" \\nas\public\game_backups2\skyrim\config /z /s /r:2 /copy:dt /w:2 /purge
+robocopy "C:\Program Files (x86)\Steam\steamapps\common\Skyrim Special Edition" \\nas\public\game_backups2\skyrim2\config /z /s /r:2 /copy:dt /w:2 /purge
 
 robocopy "C:\Users\trave\OneDrive\Documents\My Games" \\nas\public\game_backups\game_saves /z /s /r:2 /copy:dt /w:2 /purge
