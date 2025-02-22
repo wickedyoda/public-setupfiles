8d0231620d447099855cd3151e0daed6f8df688f
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2025-02-22 16:37:20 -0600

    Update Brandons-Minecraft_backup.sh
    df

--- copy-move-files/Brandons-Minecraft_backup.sh
+++ copy-move-files/Brandons-Minecraft_backup.sh
@@ -1,28 +1,2 @@
-@echo off
-setlocal
-
-:: Define Source and Destination
-set "SOURCE=C:\Users\yates\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang"
-set "DEST=\\nas\public\game_backups\Minecraft\Brandons-backup"
-
-:: Map Network Drive
-net use Z: \\nas\public /user:traver /persistent:yes
-
-:: Check if the network drive was successfully mapped
-if %ERRORLEVEL% neq 0 (
-    echo Failed to connect to NAS. Check network access and credentials.
-    exit /b 1
-)
-
-:: Ensure destination exists
-if not exist "%DEST%" mkdir "%DEST%"
-
 :: Run Robocopy to Move Files
-robocopy "%SOURCE%" "%DEST%" /z /s /r:2 /w:3 /XD
-
-:: Unmap Network Drive
-net use Z: /delete /yes
-
-echo Backup completed successfully!
-pause
-endlocal
+robocopy C:\Users\yates\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang \\nas\public\game_backups\Minecraft\Brandons-backup /z /s /r:2 /w:3 /XD /purge
