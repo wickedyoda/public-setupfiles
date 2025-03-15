f22a99e54028d900dc79c3d34edd61d4bc0caa49
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2025-03-15 16:54:12 -0500

    Updated robcopies

--- batch_files/Sync Private Folders.bat
+++ batch_files/Sync Private Folders.bat
@@ -3,8 +3,8 @@ net use s: \\nas\downloadedmedia
 net use t: \\nas\VM-proxmox
 net use r: \\nas\downloads
 
-robocopy "C:\Users\Traver\OneDrive\Personal Vault" "W:\Traver's Personal\Private files\Super Private Files" /z /s /r:2 /w:2 /copy:dt 
-robocopy "C:\Users\Trave\OneDrive\Personal Vault" "W:\Traver's Personal\Private files\Super Private Files" /z /s /r:2 /w:2 /copy:dt
+robocopy "C:\Users\Traver\OneDrive\Personal Vault" "W:\Traver's Personal\Private files\Super Private Files2" /z /s /r:2 /w:2 /copy:dt 
+robocopy "C:\Users\Trave\OneDrive\Personal Vault" "W:\Traver's Personal\Private files\Super Private Files2" /z /s /r:2 /w:2 /copy:dt
 
 
 
