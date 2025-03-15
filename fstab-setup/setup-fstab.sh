c11ad849edb4c779c72352d419130f227ed409ae
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2025-03-15 18:42:40 -0500

    Update setup-fstab.sh

--- fstab-setup/setup-fstab.sh
+++ fstab-setup/setup-fstab.sh
@@ -21,9 +21,14 @@ mkdir naspublic
 mkdir naspublic-share2
 
 # Echo the command below into the file /etc/fstab
-echo '//nas/public  /media/naspublic  cifs  vers=2.0,username=admin,password=$PASSWORD,file_mode=0777,dir_mode=0777,_netdev,auto 0 0
-//nas/DownloadedMedia  /media/nasmedia  cifs  vers=2.0,username=admin,password=$PASSWORD,file_mode=0777,dir_mode=0777,_netdev,auto 0 0
-//nas/public-share2 /media/naspublic-share2  cifs  vers=2.0,username=admin,password=$PASSWORD,file_mode=0777,dir_mode=0777,_netdev,auto 0 0' | sudo tee -a /etc/fstab
+echo '# NAS Public Share
+//nas/Public /mnt/naspublic cifs credentials=/root/.smbcredentials,vers=2.0,file_mode=0777,dir_mode=0777,_netdev,x-systemd.automount,nofail 0 0
+
+# NAS Media Share
+//nas/media /mnt/nasmedia cifs credentials=/root/.smbcredentials,vers=2.0,file_mode=0777,dir_mode=0777,_netdev,x-systemd.automount,nofail 0 0
+
+# NAS Public2 Share
+//nas/public2 /mnt/naspublic-share2 cifs credentials=/root/.smbcredentials,vers=2.0,file_mode=0777,dir_mode=0777,_netdev,x-systemd.automount,nofail 0 0' | sudo tee -a /etc/fstab
 
 # Mount the file systems
 mount -a
