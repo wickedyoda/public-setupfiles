5f1a94e8061140c1021b16c5e4ca0b2cbe491881
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2025-04-14 14:06:43 -0500

    Update ABC---Backup VM's.bat

--- batch_files/ABC---Backup VM's.bat
+++ batch_files/ABC---Backup VM's.bat
@@ -1,7 +1,8 @@
 net use w: \\nas\public
 net use s: \\nas\downloadedmedia 
 net use r: \\nas\downloads 
+net use t: \\nas\public2
 
 
 
-robocopy C:\Users\trave\Downloads\VMs \\nas\public2\vm\abcwhyme /z /s /r:2 /w:2 /copy:dt /compress
+robocopy C:\Users\trave\Downloads\VMs \\nas\public2\vm\abcwhyme /z /s /r:2 /w:2 /copy:dt /compress /purge
