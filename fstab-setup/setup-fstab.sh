a8793ddf9b5a4d5c16ee1082f34747e0ad6d97e2
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2025-05-11 19:04:17 -0500

    Update setup-fstab.sh

--- fstab-setup/setup-fstab.sh
+++ fstab-setup/setup-fstab.sh
@@ -38,13 +38,13 @@ mkdir naspublic-share2
 
 # Echo the command below into the file /etc/fstab
 echo '# NAS Public Share
-//nas/Public /mnt/naspublic cifs credentials=/root/.smbcredentials,vers=2.0,file_mode=0777,dir_mode=0777,_netdev,x-systemd.automount,nofail 0 0
+//nas/public /mnt/naspublic cifs credentials=/root/.smbcredentials,vers=3.0,file_mode=0777,dir_mode=0777,_netdev,x-systemd.automount,nofail 0 0
 
 # NAS Media Share
-//nas/media /mnt/nasmedia cifs credentials=/root/.smbcredentials,vers=2.0,file_mode=0777,dir_mode=0777,_netdev,x-systemd.automount,nofail 0 0
+//nas/media /mnt/nasmedia cifs credentials=/root/.smbcredentials,vers=3.0,file_mode=0777,dir_mode=0777,_netdev,x-systemd.automount,nofail 0 0
 
 # NAS Public2 Share
-//nas/public2 /mnt/naspublic-share2 cifs credentials=/root/.smbcredentials,vers=2.0,file_mode=0777,dir_mode=0777,_netdev,x-systemd.automount,nofail 0 0' | sudo tee -a /etc/fstab
+//nas/public2 /mnt/naspublic-share2 cifs credentials=/root/.smbcredentials,vers=3.0,file_mode=0777,dir_mode=0777,_netdev,x-systemd.automount,nofail 0 0' | sudo tee -a /etc/fstab
 
 # Mount the file systems
 mount -a
